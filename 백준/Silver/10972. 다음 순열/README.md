2323de596573cfb642c6c0b64040251a5a5eeec0
eunhxa <42601724+eunhxa@users.noreply.github.com>
2022-07-05 02:24:12 +0900

    [Silver III] Title: 다음 순열, Time: 208 ms, Memory: 17420 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/README.md
@@ -0,0 +1,12 @@
+# [Silver III] 다음 순열 - 10972 
+
+[문제 링크](https://www.acmicpc.net/problem/10972) 
+
+### 성능 요약
+
+메모리: 17420 KB, 시간: 208 ms
+
+### 분류
+
+조합론(combinatorics), 수학(math)
+
---
+++ 백준/Silver/10972. 다음 순열/다음 순열.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        int j = -1;
+        for(int i=n-1; i>=1; i--) {
+            if(arr[i] > arr[i-1]) {
+                j = i-1;
+                break;
+            }
+        }
+        if(j != -1) {
+            for(int i=n-1; i>j; i--) {
+                if(arr[i] > arr[j]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                    break;
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<=j; i++) {
+                sb.append(arr[i]+" ");
+            }
+            for(int i=n-1; i>j; i--) {
+                sb.append(arr[i]+" ");
+            }
+            System.out.println(sb);
+        } else {
+            System.out.println("-1");
+        }
+    }
+}
