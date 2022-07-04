5efa2eac0da1b612180d040f6604d1d4982d2d09
eunhxa <42601724+eunhxa@users.noreply.github.com>
2022-07-04 14:11:28 +0900

    [Silver III] Title: N과 M (3), Time: 720 ms, Memory: 212196 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    static boolean[] arr;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        arr = new boolean[n+1];
+        result = new int[m];
+
+        go(0, n, m);
+        System.out.println(sb);
+    }
+
+    public static void go(int index, int n, int m) {
+        if(index == m) {
+            for(int i=0; i<m; i++) {
+                sb.append(result[i]+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i=1; i<=n; i++) {
+            arr[i] = true;
+            result[index] = i;
+            go(index+1, n, m);
+            arr[i] = false;
+        }
+    }
+}
